f50ab7a26683569fe22dd7409a0b15fb4baa4438
Vitaly Orekhov <vkvo2000@vivaldi.net>
2026-01-10 18:29:00 +0300

    Update github-pr-author.user.js

--- github-pr-author.user.js
+++ github-pr-author.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Github improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.17
+// @version      0.18
 // @updateURL    https://raw.githubusercontent.com/learn-more/tampermonkey/master/github-pr-author.user.js
 // @downloadURL  https://raw.githubusercontent.com/learn-more/tampermonkey/master/github-pr-author.user.js
 // @description  Various github improvements, like: show committer and author name, 'known' authors, etc...
